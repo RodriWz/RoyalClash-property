7d726775581ceb036536d22bc017edcb02efb210
Rodri <angrysoul983@gmail.com>
2025-07-31 11:46:13 +0800

    style: Perbesar font size jadi 40px di elemen tertentu feat: Tambah script JS untuk animasi scroll saat navigasi halaman

--- js/script.js
+++ js/script.js
@@ -8,6 +8,63 @@ ToggleBtnId.addEventListener("click", () => {
 });
 
 // Hide Menu
-ClaseBtnId.addEventListener("click", () => {
+CloseBtnId.addEventListener("click", () => {
     navId.classList.remove("show");
 });
+
+AOS.init();
+
+// Logo
+gsap.from(".logo",{
+    opacity:0,
+    y:-10,
+    delay:1,
+    duration:0.5,
+})
+
+// nav menu
+gsap.from(".nav_menu_list .nav_menu_item",{
+    opacity:0,
+    y:-10,
+    delay:1,
+    duration:0.5,
+    stagger:0.3,
+})
+
+// toggle btn
+gsap.from(".toggle_btn",{
+    opacity:0,
+    y:-10,
+    delay:1,
+    duration:0.5,
+})
+
+// main heading
+gsap.from(".main-heading",{
+    opacity:0,
+    y:20,
+    delay:2,
+    duration:0.5,
+})
+
+// btn 
+gsap.from(".btn_wrapper",{
+    opacity:0,
+    y:20,
+    delay:1.8,
+    duration:0.5,
+})
+
+gsap.from(".team_img_wrapper img",{
+    opacity:0,
+    y:20,
+    delay:1.8,
+    duration:0.5,
+})
+
+gsap.from(".info-text",{
+    opacity:0,
+    y:20,
+    delay:1.8,
+    duration:0.5,
+})
